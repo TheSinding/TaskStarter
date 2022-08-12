ebabb7cc9ed6719b75fc0683b97ec27b3676df08
Simon Sinding <simon.sinding@danfoss.com>
2022-08-12 11:09:43 +0200

    chore: prettier does it's magic

--- src/commands/StartTask/types.ts
+++ src/commands/StartTask/types.ts
@@ -1,27 +1,28 @@
-import { UserIdentityRef } from "azure-devops-node-api/interfaces/GalleryInterfaces";
-import { WorkItem as _WorkItem } from "azure-devops-node-api/interfaces/WorkItemTrackingInterfaces";
-import { QuickPickItem, Uri } from "vscode";
-import { WorkItemType } from "../../@types/VscodeTypes";
+import { UserIdentityRef } from 'azure-devops-node-api/interfaces/GalleryInterfaces'
+import { WorkItem as _WorkItem } from 'azure-devops-node-api/interfaces/WorkItemTrackingInterfaces'
+import { QuickPickItem, Uri } from 'vscode'
+import { WorkItemType } from '../../@types/VscodeTypes'
 export interface UserInfo extends UserIdentityRef {
-	_links?: {
-		avatar?: { href?: string }
-	}
-	uniqueName?: string
-	imageUrl?: string
-	descriptor?: string
+  _links?: {
+    avatar?: { href?: string }
+  }
+  uniqueName?: string
+  imageUrl?: string
+  descriptor?: string
 }
 
-export const fields = ["System.Title",
-	"System.Id",
-	"System.AssignedTo",
-	"System.State",
-	"System.WorkItemType",
-	"Microsoft.VSTS.Scheduling.RemainingWork",
-	"System.Parent"
-] as const;
+export const fields = [
+  'System.Title',
+  'System.Id',
+  'System.AssignedTo',
+  'System.State',
+  'System.WorkItemType',
+  'Microsoft.VSTS.Scheduling.RemainingWork',
+  'System.Parent',
+] as const
 
-type WorkItemField = typeof fields[number];
+export type WorkItemField = typeof fields[number]
 
-export type WorkItem = _WorkItem & { fields?: Record<WorkItemField, any> };
+export type WorkItem = _WorkItem & { fields?: Record<WorkItemField, any> }
 
-export type TaskPick = QuickPickItem & { taskUri?: Uri, taskType?: WorkItemType };
+export type TaskPick = QuickPickItem & { taskUri?: Uri; taskType?: WorkItemType }
