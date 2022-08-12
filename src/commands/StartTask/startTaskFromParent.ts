294b31a4d7717926f70787a1b6ec958ef63f4967
Simon Sinding <simon.sinding@danfoss.com>
2022-08-12 11:10:34 +0200

    feat: adds a "open on devops" from parents list

--- src/commands/StartTask/startTaskFromParent.ts
+++ src/commands/StartTask/startTaskFromParent.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { commands, env, ThemeIcon, Uri, window } from 'vscode'
+import { commands, ThemeIcon, Uri, window } from 'vscode'
 import { logger } from '../../logger'
 import { TaskPick, UserInfo, WorkItem } from './types'
 import { listParents } from './api'
 import { WorkItemType } from '../../@types/VscodeTypes'
 import { getWorkItemIcon } from './utils'
 import { COMMAND as startTaskCommand } from './startTask'
+import { COMMAND as openOnDevOpsCommand } from '../openOnDevOps'
 import { createQuickPickHelper } from '../../utils/createQuickPickHelper'
-import { getProjectKey } from '../../configuration'
 
 export const COMMAND = 'taskstarter.startTaskFromParent'
 
@@ -43,7 +43,7 @@ const commandHandler = async () => {
     picker.busy = false
 
     picker.onDidAccept(() => next(picker.selectedItems))
-    picker.onDidTriggerItemButton(({ item }) => item.taskUri && env.openExternal(item.taskUri))
+    picker.onDidTriggerItemButton(({ item }) => commands.executeCommand(openOnDevOpsCommand, item.description))
   } catch (error: any) {
     logger.error(error)
     window.showErrorMessage(error.message)
@@ -51,8 +51,6 @@ const commandHandler = async () => {
 }
 
 const itemMapper = (workItem: WorkItem): TaskPick => {
-  const organization = getProjectKey('devopsOrganization')
-  const project = getProjectKey('devopsProject')
   const assignedTo: UserInfo = workItem.fields?.['System.AssignedTo']
   const itemType: WorkItemType = workItem.fields?.['System.WorkItemType']
   const taskState = workItem?.fields?.['System.State']
@@ -60,13 +58,8 @@ const itemMapper = (workItem: WorkItem): TaskPick => {
   const assignedToText = `Assigned to: ${assignedTo && assignedTo.displayName ? assignedTo.displayName : 'None'}`
   const taskTypeText = `Type: ${itemType ? itemType : 'Unknown'}`
   const taskStateText = `State: ${taskState ? taskState : 'Unknown'}`
-  const buttons = []
+  const buttons = [{ iconPath: new ThemeIcon('open-editors-view-icon'), tooltip: 'View on DevOps' }]
   let taskUri: Uri | undefined
-
-  if (organization && project) {
-    taskUri = Uri.parse(`https://${organization}.visualstudio.com/${project}/_workitems/edit/${workItem.id}/`)
-    buttons.push({ iconPath: new ThemeIcon('open-editors-view-icon'), tooltip: 'View on DevOps' })
-  }
 
   return {
     label: `$(${getWorkItemIcon(itemType)}) ${workItem.fields!['System.Title']}`,
