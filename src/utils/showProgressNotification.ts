ff94b0db6f37b2f98b9ba982e86199e70a7bf679
Simon Sinding <simon.sinding@danfoss.com>
2022-08-25 13:41:13 +0200

    feat: adds a show progress notification popup helper

---
+++ src/utils/showProgressNotification.ts
@@ -0,0 +1,8 @@
+import { ProgressLocation, window } from "vscode";
+
+export const showProgressNotification = <T>(message: string, cb: Promise<T>): Promise<T> => {
+	return window.withProgress({ location: ProgressLocation.Notification }, (p) => {
+		p.report({ message })
+		return cb
+	}) as Promise<T>
+}
